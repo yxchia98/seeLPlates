87262b884919d6f48e7cd5cac2d0067d80b36427
yixuan.chia <yixuan.chia@dell.com>
2023-06-19 14:43:20 +0000

    edited training params

--- train.py
+++ train.py
@@ -18,15 +18,19 @@ path = os.path.join(location, dir)
 if os.path.exists(path):
     shutil.rmtree(path)
 
-input_model = os.environ.get('model') + '.pt' if os.environ.get('model') else 'yolov8n.pt'
+# input_model = os.environ.get('model') + '.pt' if os.environ.get('model') else 'yolov8n.pt'
+input_model = 'yolov5m.pt'
+
 # Load a model
 # model = YOLO('yolov8n.yaml')  # build a new model from YAML
 model = YOLO(input_model)  # load a pretrained model (recommended for training)
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
-epochs = os.environ.get('epochs') if os.environ.get('epochs') else 10
-device = os.environ.get('device') if os.environ.get('device') else 'cpu'
+# epochs = os.environ.get('epochs') if os.environ.get('epochs') else 100
+# device = os.environ.get('device') if os.environ.get('device') else 'cpu'
+epochs = 100
+device = '0'
 # Train the model
-model.train(data='lpd.yaml', epochs=200, imgsz=640, batch=8, device='cpu')
+model.train(data='lpd.yaml', epochs=epochs, imgsz=640, device=device)
 
 # Source path 
 src = '/cnvrg/runs/detect/train'
