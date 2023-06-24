9e16a87df27a5037a4d50918f7f90f56e502c40b
yixuan.chia <yixuan.chia@dell.com>
2023-06-24 07:13:09 +0000

    fixed CLI

--- train.py
+++ train.py
@@ -16,9 +16,9 @@ import argparse
 # Takes in CLI arguments
 argParser = argparse.ArgumentParser(description="set parameters for YOLOv8 model training")
 argParser.add_argument("-e", "--epochs", default=50, type=int, help="number of epochs to be used, default to 50")
-argParser.add_argument("-b", "--batch", default="16", help="specify batch size, default to 16")
+argParser.add_argument("-b", "--batch", default="16", type=int, help="specify batch size, default to 16")
 argParser.add_argument("-m", "--model", default="yolov8n.pt", help="YOLOv8 model to be used: yolov8n.pt, yolov8s.pt, yolov8m.pt. defaults to yolov8n.pt")
-args = parser.parse_args()
+args = argParser.parse_args()
 
 ultralytics.checks()
 
