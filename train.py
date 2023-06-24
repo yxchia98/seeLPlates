ea41f6dbbeccd6257747712982b2ec63442ee923
Lulmao <yxchia98@gmail.com>
2023-06-24 15:04:16 +0800

    added cli

--- train.py
+++ train.py
@@ -10,8 +10,15 @@ from matplotlib import pyplot as plt
 import os
 import subprocess
 import torch
+import argparse
 
 
+# Takes in CLI arguments
+argParser = argparse.ArgumentParser(description="set parameters for YOLOv8 model training")
+argParser.add_argument("-e", "--epochs", default=50, type=int, help="number of epochs to be used, default to 50")
+argParser.add_argument("-b", "--batch", default="16", help="specify batch size, default to 16")
+argParser.add_argument("-m", "--model", default="yolov8n.pt", help="YOLOv8 model to be used: yolov8n.pt, yolov8s.pt, yolov8m.pt. defaults to yolov8n.pt")
+args = parser.parse_args()
 
 ultralytics.checks()
 
@@ -23,7 +30,10 @@ if os.path.exists(path):
     shutil.rmtree(path)
 
 # input_model = os.environ.get('model') + '.pt' if os.environ.get('model') else 'yolov8n.pt'
-input_model = 'yolov8s.pt'
+# input_model = 'yolov8s.pt'
+input_model = args.model
+batch_size = args.batch
+epochs = args.epochs
 
 # Load a model
 # model = YOLO('yolov8n.yaml')  # build a new model from YAML
@@ -31,9 +41,7 @@ model = YOLO(input_model)  # load a pretrained model (recommended for training)
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 # epochs = os.environ.get('epochs') if os.environ.get('epochs') else 100
 # device = os.environ.get('device') if os.environ.get('device') else 'cpu'
-epochs = 100
 device = '0' if torch.cuda.is_available() else 'cpu'
-batch_size = 32
 # Train the model
 model.train(data='lpd.yaml', epochs=epochs, imgsz=640, batch=batch_size, device=device)
 
